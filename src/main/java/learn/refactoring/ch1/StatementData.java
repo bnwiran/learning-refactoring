76ff11104e07a07bc37110c40593734c4b1f08d7
Belal Ahmad Nwiran <bnwiran@gmail.com>
2024-06-27 19:12:55 -0400

    Update StatementData class

---
+++ src/main/java/learn/refactoring/ch1/StatementData.java
@@ -0,0 +1,10 @@
+package learn.refactoring.ch1;
+
+import org.json.JSONArray;
+
+class StatementData {
+    String customer;
+    JSONArray performances;
+    double totalAmount;
+    double totalVolumeCredits;
+}
