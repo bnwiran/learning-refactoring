b4c1f9bbb3913339ed4ec3663d2ddeabd1e496ec
Belal Ahmad Nwiran <bnwiran@gmail.com>
2024-06-27 19:52:42 -0400

    Override amount method in PerformanceCalculator

--- src/main/java/learn/refactoring/ch1/TragedyCalculator.java
+++ src/main/java/learn/refactoring/ch1/TragedyCalculator.java
@@ -6,4 +6,16 @@ public class TragedyCalculator extends PerformanceCalculator {
     public TragedyCalculator(JSONObject performance, JSONObject play) {
         super(performance, play);
     }
+
+    @Override
+    public int amount() {
+        var audience = performance.getInt("audience");
+        var result = 40_000;
+
+        if (audience > 30) {
+            result += 1000 * (audience -  30);
+        }
+
+        return result;
+    }
 }
