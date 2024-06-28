6a91f848cc2362a884813925e5d132589948d99f
Belal Ahmad Nwiran <bnwiran@gmail.com>
2024-06-28 18:47:38 -0400

    CH4: Add noProducers test

--- src/test/java/learn/refactoring/ch4/ProvinceTest.java
+++ src/test/java/learn/refactoring/ch4/ProvinceTest.java
@@ -1,6 +1,5 @@
 package learn.refactoring.ch4;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,32 +7,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProvinceTest {
 
-    private Province province;
+    private Province asia;
 
     @BeforeEach
     public void setUp() {
-        province = getSampleProvince();
+        asia = getSampleProvince("Asia");
     }
 
     @Test
     public void shortfall() {
-        assertEquals(5, province.shortfall());
+        assertEquals(5, asia.shortfall());
     }
 
     @Test
     public void profit() {
-        assertEquals(230, province.profit());
+        assertEquals(230, asia.profit());
     }
 
     @Test
     public void changeProduction() {
-        province.producers().getFirst().production(20);
-        assertEquals(-6, province.shortfall());
-        assertEquals(292, province.profit());
+        asia.producers().getFirst().production(20);
+        assertEquals(-6, asia.shortfall());
+        assertEquals(292, asia.profit());
     }
 
-    private static Province getSampleProvince() {
-        var name = "Asia";
+    @Test
+    public void noProducers() {
+        var noProducers = new Province("No producers", 30, 20);
+        assertEquals(30, noProducers.shortfall());
+        assertEquals(0, noProducers.profit());
+    }
+
+    private static Province getSampleProvince(String name) {
         var demand = 30;
         var price = 20;
         var province = new Province(name, demand, price);
